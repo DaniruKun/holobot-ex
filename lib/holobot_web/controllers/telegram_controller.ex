63583d9dc1050e3bb92b800f6d2bf7ccd69b8978
Daniils Petrovs <dpetrovs@evolution.com>
2020-12-19 17:25:12 +0200

    Implement basic telegram controller action

--- lib/holobot_web/controllers/telegram_controller.ex
+++ lib/holobot_web/controllers/telegram_controller.ex
@@ -1,9 +1,13 @@
 defmodule HolobotWeb.TelegramController do
   use HolobotWeb, :controller
-  import Logger
 
-  def update(conn, params) do
-    info("Received update from Telegram", [])
+  require Logger
+
+  def update(conn, _params) do
+    Logger.info("Received update from Telegram", [])
+    {:ok, body, conn} = Plug.Conn.read_body(conn, length: 100_000)
+    decoded_body = Jason.decode!(body)
+    Logger.info(decoded_body, [])
+    text(conn, "Ok")
   end
-
 end
