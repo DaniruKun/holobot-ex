ab2c6a59fef451804e4566b5d747b88dbd24efe2
Daniils Petrovs <dpetrovs@evolution.com>
2020-12-24 19:37:36 +0200

    Implement videos context

---
+++ lib/holobot/holofans/videos.ex
@@ -0,0 +1,26 @@
+defmodule Holobot.Holofans.Videos do
+  @moduledoc """
+  Holofans API videos context.
+  """
+
+  @holofans_api "https://api.holotools.app/v1"
+
+  require Finch
+
+  def get_videos(attrs) do
+    qparams = URI.encode_query(attrs)
+    req = Finch.build(:get, @holofans_api <> "/videos?#{qparams}")
+    {:ok, resp} = Finch.request(req, HolofansAPIClient)
+    {:ok, decoded_body} = Jason.decode(resp.body)
+    decoded_body
+  end
+
+  def get_video(yt_video_id, attrs \\ %{with_comments: 0}) do
+    qparams = URI.encode_query(attrs)
+    req = Finch.build(:get, @holofans_api <> "/videos/youtube/#{yt_video_id}?#{qparams}")
+    {:ok, resp} = Finch.request(req, HolofansAPIClient)
+    {:ok, decoded_body} = Jason.decode(resp.body)
+    decoded_body
+  end
+
+end
