da060f9005985bef2d7946e3bdf504d5af81dc33
Daniils Petrovs <dpetrovs@evolution.com>
2020-12-26 00:48:08 +0200

    Implement list of live streams sender

--- lib/holobot/telegram/chat.ex
+++ lib/holobot/telegram/chat.ex
@@ -8,24 +8,43 @@ defmodule Holobot.Telegram.Chat do
 
   @yt_vid_url_base "https://www.youtube.com/watch?v="
 
-  def send_live_list(lives) when is_list(lives) do
-    msg_body =
+  @spec send_live_list!(integer | binary, maybe_improper_list) :: {:ok, Nadia.Model.Message.t()}
+  def send_live_list!(chat_id, lives) when is_list(lives) do
+    live_channels_body =
       lives
-      |> Enum.each(fn live ->
-        "*#{live.channel.name}* [#{@yt_vid_url_base}#{live.yt_video_key}](#{live.title})\n"
-      end)
+      |> Stream.map(&build_live_msg_entry/1)
+      |> Enum.join()
 
-    Logger.info("Message body:\n#{msg_body}")
+    msg = "*Currently live channels:*\n\n" <> live_channels_body
+
+    Logger.debug("Message body:\n#{live_channels_body}")
+
+    {:ok, _} = Nadia.send_message(chat_id, msg, [
+      {:parse_mode, "Markdown"},
+      {:disable_web_page_preview, true}
+    ])
   end
 
-  @spec send_help_msg(integer | binary) :: {:error, Nadia.Model.Error.t()} | {:ok, Nadia.Model.Message.t()}
-  def send_help_msg(chat_id) do
+  @spec send_help_msg!(integer | binary) ::
+          {:error, Nadia.Model.Error.t()} | {:ok, Nadia.Model.Message.t()}
+  def send_help_msg!(chat_id) do
     msg =
       "**A-Chan** is a helpful Hololive bot that provides information about Hololive videos, live streams and channels!
 
 Some commands you can use:
 `/live [time]` - get list of livestreams that either already started or will start `[time]` into the future, e.g. `/live now`"
 
-    Nadia.send_message(chat_id, msg, [{:parse_mode, "Markdown"}])
+    {:ok, _} = Nadia.send_message(chat_id, msg, [{:parse_mode, "Markdown"}])
+  end
+
+  defp build_live_msg_entry(live) do
+    %{"channel" => %{"name" => name}, "yt_video_key" => yt, "title" => title} = live
+
+    clean_title =
+      title
+       |> String.replace("[", "|")
+       |> String.replace("]", "|")
+
+    "#{name} [#{clean_title}](#{@yt_vid_url_base}#{yt})\n"
   end
 end
