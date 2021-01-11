afbf3da04bc4e4dacb00b4c213cb62bac537b7e5
Daniils Petrovs <dpetrovs@evolution.com>
2021-01-11 03:45:44 +0200

    Rebuild parser, move to helpers module

--- lib/holobot/helpers.ex
+++ lib/holobot/helpers.ex
@@ -5,6 +5,20 @@ defmodule Holobot.Helpers do
   Functions that are not strictly related to Holofans or Telegram domains.
   """
 
+  @about_cover "COVER corp. is a cutting edge 2D entertainment company. It is the parent company of Hololive Production."
+  @about_hololive "Hololive Production is a virtual YouTuber talent agency."
+  @about_yagoo """
+  YAGOO (Motoaki Tanigo) is the CEO of COVER Corp.
+
+  Originally focusing on the development and creation of AR and VR technologies, in 2017, COVER Corp. created the talent agency, hololive production.
+
+  He likes to watch TV and YouTube before going to bed, sometimes falling asleep on the sofa.
+
+  The name YAGOO originated from one of Roboco's streams, where Subaru misread "Tanigo" as "Yagoo".
+  """
+
+  @type question_type() :: :concept_compl | :verification | :quantification
+
   @doc """
   Get the emoji corresponding to a Hololive channel by the YT channel ID.
   Returns empty binary if not found.
@@ -85,5 +99,85 @@ defmodule Holobot.Helpers do
     |> String.replace(~r/[[:punct:]]/, "")
     |> String.downcase()
     |> String.split()
+  end
+
+  @doc """
+  Parse a tokenized Telegram message sentence and generate a response.
+  """
+  @spec parse(list(binary())) :: binary()
+  def parse(tokens) do
+    # This is mostly just a placeholder dead simply NLP pipeline,
+    # the idea is to possibly integrate a sophisticated cloud NLP service
+    # like Amazon Lex in here to generate responses.
+    if is_greeting?(tokens) do
+      random_greeting()
+    else
+      # It is a question or anything else
+      case classify_question_type(tokens) do
+        :verification ->
+          answer_y_n(tokens)
+
+        :concept_compl ->
+          define_concept(tokens)
+
+        _ ->
+          "I don't know"
+      end
+    end
+  end
+
+  defp define_concept(tokens) do
+    cond do
+      "hololive" in tokens -> @about_hololive
+      "yagoo" in tokens -> @about_yagoo
+      "cover" in tokens -> @about_cover
+      true -> "I don't know who or what that is."
+    end
+  end
+
+  defp answer_y_n(tokens) do
+    cond do
+      "you" in tokens and "real" in tokens ->
+        "Of course I am!"
+
+      "anyone" in tokens and "streaming" in tokens and "now" in tokens ->
+        "Someone might be streaming right now..."
+
+      true ->
+        "I can neither confirm nor deny that."
+    end
+  end
+
+  defp is_greeting?(tokens) do
+    greetings = ["hello", "こんにちは", "hi"]
+
+    Enum.any?(tokens, &Enum.member?(greetings, &1))
+  end
+
+  defp random_greeting() do
+    greetings = [
+      "Hello!",
+      "How you doing?",
+      "Hey!"
+    ]
+
+    Enum.random(greetings)
+  end
+
+  @spec classify_question_type(list(binary())) :: question_type()
+  defp classify_question_type(tokens) do
+    cond do
+      Enum.any?(tokens, &Enum.member?(["what", "who", "where", "when"], &1)) ->
+        :concept_compl
+
+      "is" in tokens or "are" in tokens ->
+        :verification
+
+      "how" in tokens and ("much" in tokens or "many" in tokens) ->
+        :quantification
+
+      true ->
+        nil
+    end
   end
 end
