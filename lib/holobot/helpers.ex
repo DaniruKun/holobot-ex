eb17450500ac991ff879a285890e6c478874da2d
Daniils Petrovs <daniils@platogo.com>
2021-12-20 22:57:37 +0100

    Add HoloX emojis

--- lib/holobot/helpers.ex
+++ lib/holobot/helpers.ex
@@ -82,7 +82,13 @@ defmodule Holobot.Helpers do
       "UC3n5uGu18FoCy23ggWWp8tA" => "🪶",
       "UCmbs8T6MWqUHP1tIQvSgKrg" => "⏳",
       "UCO_aKKYxn4tvrqPjcTzZ6EQ" => "🌿",
-      "UCgmPnx-EEeOrZSg5Tiw7ZRQ" => "🎲"
+      "UCgmPnx-EEeOrZSg5Tiw7ZRQ" => "🎲",
+      # HoloX
+      "UCENwRMx5Yh42zWpzURebzTw" => "🛸💜",
+      "UCs9_O1tRPMQTHQ-N_L6FU2g" => "🥀",
+      "UC6eWCld0KwmyHFbAqK3V-Rw" => "🧪",
+      "UCIBY1ollUsauvVi4hW4cumw" => "🎣",
+      "UC_vMYWcDjmfdpH6r4TTn1MQ" => "🍃"	
     }
 
     Map.get(channel_emoji, yt_channel_id, "")
